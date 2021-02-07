0ab9a43cf8e78d90f1de2128a67655d3c435fe3b
MohamadKayal <34998987+MohamadKayal@users.noreply.github.com>
2021-02-08 01:11:53 +0400

    removing error handling

--- index.js
+++ index.js
@@ -7,47 +7,41 @@ function searchRepos(query, startCallback, callback) {
   if (startCallback) {
     startCallback();
   }
-
+  
   const response = fetch(getGithubRepoSearchUrl(query))
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
       if (callback) {
         callback(data);
       }
     })
     .catch((err) => {
-      toggleError(true, 'Sorry, something went wrong, please, try again later.');
+      toggleError(true,err);
     });
 }
-const searchInput = document.querySelector('#input');
-const listElement = document.querySelector('#response');
-const loadingElement = document.querySelector('#loading');
-const errorElement = document.querySelector('#error');
-const showingResultFor = document.querySelector('#showingResultsFor');
-const searchForm = document.querySelector('#form');
-const errMessage = document.createElement('h4');
+const searchInput = document.querySelector("#input");
+const listElement = document.querySelector("#response");
+const loadingElement = document.querySelector("#loading");
+const errorElement = document.querySelector("#error");
+const showingResultFor = document.querySelector("#showingResultsFor"); 
+const searchForm = document.querySelector("#form");
+const errMessage = document.createElement('p');
 const toggleLoading = (show = false) => {
   loadingElement.style.display = show ? 'block' : 'none';
 };
-
+  
 const toggleError = (show = false, errorMessage) => {
-  showResultsFor();
   errMessage.innerText = errorMessage;
-  clearToggles(true, true);
+  clearToggles(true,true);
   // toggleLoading(false);
   errorElement.appendChild(errMessage);
-  errorElement.style.display = show ? 'block' : 'none';
+  errorElement.style.display =  show ? 'block' : 'none';
 };
 
-const appendRepo = ({
-  owner: { html_url: url },
-  owner: { avatar_url },
-  full_name: fullName,
-  html_url: htmlUrl,
-  description,
-}) => {
+const appendRepo = ({ owner: { html_url:url}, owner: {avatar_url},full_name: fullName, html_url: htmlUrl, description }) => {
+ 
   const repoElement = document.createElement('li');
-
+  
   const userPersonalPic = document.createElement('img');
   userPersonalPic.src = avatar_url;
   repoElement.appendChild(userPersonalPic);
@@ -58,7 +52,7 @@ const appendRepo = ({
   const repoNameElement = document.createElement('p');
   repoNameElement.innerText = fullName;
   repoElement.appendChild(repoNameElement);
-
+  
   const repoUserUrlElem = document.createElement('a');
   repoUserUrlElem.href = url;
   repoUserUrlElem.innerText = 'User Profile';
@@ -68,34 +62,32 @@ const appendRepo = ({
 
   const repoUrlElem = document.createElement('a');
   repoUrlElem.href = htmlUrl;
-  repoUrlElem.innerText = "Repository's home page";
+  repoUrlElem.innerText = 'Repository\'s home page';
   repoElement.appendChild(repoUrlElem);
 
   listElement.appendChild(repoElement);
 };
 
-function makeSearch() {
-  // combining the onClick with the form
-  searchRepos(
-    searchInput.value,
-    () => {
-      toggleLoading(true);
-      listElement.innerHTML = '';
-      clearToggles(false, true);
-    },
-    ({ items }) => {
-      showResultsFor(true, searchInput.value);
-      toggleLoading();
-      searchForm.reset();
-      items.forEach((item) => appendRepo(item));
-    }
-  );
+function makeSearch () { // combining the onClick with the form
+  searchRepos(searchInput.value, () => {
+    toggleLoading(true);
+    listElement.innerHTML = '';
+    clearToggles(false,true);
+  },
+  ({ items }) => {
+    showResultsFor(true,searchInput.value);
+    if(!items) throw "The Request is facing an error right now.\n If the problem persists, please don't contact customer service";
+    if(items ['length'] == 0) throw "No Results";
+    toggleLoading();
+    searchForm.reset();
+    items.forEach(item => appendRepo(item));
+  });
+};
+function clearToggles(clearLoading = false ,clearError = false ){
+  if (clearLoading) loadingElement.style.display = 'none' ; 
+  if (clearError) errorElement.style.display= 'none' ;
 }
-function clearToggles(clearLoading = false, clearError = false) {
-  if (clearLoading) loadingElement.style.display = 'none';
-  if (clearError) errorElement.style.display = 'none';
-}
-function showResultsFor(show = false, textInput = ' ') {
-  showingResultFor.style.display = show ? 'block' : 'none';
+function showResultsFor(show = false, textInput = ' '){
+  showingResultFor.style.display = show? 'block' : 'none' ;
   showingResultFor.innerText = `Showing Results for: ${textInput}`;
 }
