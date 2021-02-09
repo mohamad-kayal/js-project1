4f01bb547005db5fc128d22e1607d1547e311be8
MohamadKayal <34998987+MohamadKayal@users.noreply.github.com>
2021-02-09 23:43:14 +0400

    still working1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import {getGithubRepoSearchUrl} from './connection.js';
-import {appendRepo} from './domHandler.js';
-
+import {clearList} from './domHandler.js';
 let searchInput;
 let listElement;
 let loadingElement; 
@@ -17,7 +16,40 @@ window.onload = () => {
   searchForm = document.querySelector('#form');
 };
 
+const appendRepo = ({
+  owner: { html_url: url },
+  owner: { avatar_url },
+  full_name: fullName,
+  html_url: htmlUrl,
+  description,
+}) => {
+  const repoElement = document.createElement('li');
+  const userPersonalPic = document.createElement('img');
 
+  userPersonalPic.src = avatar_url;
+  repoElement.appendChild(userPersonalPic);
+
+  const repoDescElement = document.createElement('h3');
+  repoDescElement.innerText = description;
+  repoElement.appendChild(repoDescElement);
+
+  const repoNameElement = document.createElement('p');
+  repoNameElement.innerText = fullName;
+  repoElement.appendChild(repoNameElement);
+
+  const repoUserUrlElem = document.createElement('a');
+  repoUserUrlElem.href = url;
+  repoUserUrlElem.innerText = 'User Profile';
+  repoElement.appendChild(repoUserUrlElem);
+
+  repoElement.appendChild(document.createElement('br'));
+
+  const repoUrlElem = document.createElement('a');
+  repoUrlElem.href = htmlUrl;
+  repoUrlElem.innerText = "Repository's home page";
+  repoElement.appendChild(repoUrlElem);
+  listElement.appendChild(repoElement);
+};
 
 function searchRepos(query, startCallback, callback) {
   if (startCallback) {
@@ -76,6 +108,3 @@ const toggleError = (show = false, errorMessage) => {
   errorElement.style.display = show ? 'block' : 'none';
 };
 
-function clearList() {
-  listElement.innerHTML = '';
-}
--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,5 @@ module.exports = {
         path:path.resolve(__dirname,"./dist"),
         filename: "bundle.js"
     },
-    mode: "production"
+    mode: "development"
 }
